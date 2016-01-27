eb066a17d30e1ad1b1f49c596fd0adf84bf878f3
H9964 <h9964@student.jamk.fi>
2016-01-27 19:18:54 +0200

    demo5 tehtavat

---
+++ Demo5_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle mycar = new Vehicle("Toyota", "Purkki");
+            Console.WriteLine(mycar.ToString());
+
+            mycar.RemoveTyres();
+            Console.WriteLine(mycar.ToString());
+
+            mycar.AddTyre(new Tyre("Nokia", "Hakkaapäälle", "195/65R15"));
+            mycar.AddTyre(new Tyre("Nokia", "Hakkaapäälle", "195/65R15"));
+            mycar.AddTyre(new Tyre("Nokia", "Hakkaapäälle", "195/65R15\n"));
+            Console.WriteLine(mycar.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo5_1/Vehicle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo5_1
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public void RemoveTyres()
+        {
+            tyres.Clear();
+        }
+
+        public void AddTyre(Tyre t)
+        {
+            tyres.Add(t);
+        }
+
+        public Vehicle (string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+
+            tyres = new List<Tyre>();
+
+            tyres.Add(new Tyre("Michelin", "X-Ice", "225/45R17"));
+            tyres.Add(new Tyre("Michelin", "X-Ice", "225/45R17"));
+            tyres.Add(new Tyre("Michelin", "X-Ice", "225/45R17"));
+            tyres.Add(new Tyre("Michelin", "X-Ice", "225/45R17\n"));
+
+
+        }
+
+        public override string ToString()
+        {
+            string s = "Vehicle: " + Brand + ", " + Model;
+
+            if (tyres.Count == 0)
+            {
+                s += "\nNo tyres\n ";
+            }
+            else
+            {
+                foreach (Tyre t in tyres)
+                {
+                    s += "\n";
+                    s += t.ToString();
+                }
+            }
+            return s;
+        }
+
+        private List<Tyre> tyres;
+
+    }
+}

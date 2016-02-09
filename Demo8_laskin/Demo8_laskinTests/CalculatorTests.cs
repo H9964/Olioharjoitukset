bc0a2c22fb333e21c17e2925d8c47f6d640b41ef
H9964 <h9964@student.jamk.fi>
2016-02-09 18:37:29 +0200

    laskin

---
+++ Demo8_laskin/Demo8_laskinTests/CalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo8_laskin.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            //arrange
+            Calculator calc = new Calculator();
+            int number1 = 5;
+            int number2 = 7;
+            int expected = 12;
+
+            // act
+            int actual = calc.Add(number1, number2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            int number1 = 5;
+            int number2 = 7;
+            int expected = 35;
+
+            // act
+            int actual = calc.Multiply(number1, number2);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            int number1 = 5;
+            int number2 = 7;
+            int result = number1 / number2;
+
+            // act
+            int actual = calc.Divide(number1, number2);
+
+            // assert
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
